848f152502c122d205a1069ba97f9352fee950ee
vmahal <47036560+vmahal@users.noreply.github.com>
2019-03-23 10:52:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+
+var drumButtons=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<drumButtons;i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+  var buttpressed=this.innerHTML;
+  makeSound(buttpressed);
+  animate(buttpressed);
+});
+}
+
+
+  document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    animate(event.key);
+  });
+
+
+function makeSound(key){
+
+  switch (key) {
+
+    case "w": var audio = new Audio('sounds/crash.mp3');
+                   audio.play();
+             break;
+
+    case "a": var audio1 =new Audio("sounds/kick-bass.mp3");
+                   audio1.play();
+                   break;
+
+     case "s": var audio2 = new Audio("sounds/snare.mp3");
+                   audio2.play();
+                   break;
+
+     case "d": var audio3=new Audio("sounds/tom-1.mp3");
+                   audio3.play();
+                   break;
+
+     case "j": var audio4=new Audio("sounds/tom-2.mp3");
+                   audio4.play();
+                   break;
+
+     case "k": var audio5=new Audio("sounds/tom-3.mp3");
+                   audio5.play();
+                   break;
+
+     case "l": var audio6=new Audio("sounds/tom-4.mp3");
+                   audio6.play();
+                   break;
+    default: console.log(key);
+
+  }
+}
+
+
+function animate(buttonpress){
+  var am=document.querySelector("."+buttonpress);
+  am.classList.add("pressed");
+setTimeout(function(){ am.classList.remove("pressed");
+},100);
+
+}
